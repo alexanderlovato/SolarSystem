f06ae3849474c9a022c5585e734e454ba565e240
alexanderlovato <alex.lovato88@gmail.com>
2016-04-30 00:45:31 -0600

    Master List and Detail View
    1. Deleted UIViewController from Storyboard and added a UITableViewController.
    2. Embedded a UINavigationController to the UITableViewController.
    3. Created PlanetsTableViewController file (Should have been PlanetListViewController) and implemented the PlanetController.planets array.
    4. Created another UIViewController scene to Main.Storyboard.
    5. Created PlanetDetailViewController and set the new UIViewController scene to this class.
    6. Added a Horizontal Stack View with four labels, which will use the PlanetController array values.
    7. Began adding the updateWithPlanets function and am now stuck.

---
+++ SolarSystem/PlanetDetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PlanetDetailViewController.swift
+//  SolarSystem
+//
+//  Created by Alexander Lovato on 4/29/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class PlanetDetailViewController: UIViewController {
+    @IBOutlet weak var planetImage: UIImageView!
+
+    @IBOutlet weak var planetLabels: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateWithPlanet(planetName: PlanetController) {
+        
+    
+    
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
